f8ff735d433c4ffd3a941ea76062bbaa9ddf79e2
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2026-01-28 16:09:11 +0100

    more robust config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,29 @@
 // @ts-check
 
 import eslint from "@eslint/js";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores} from "eslint/config";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  eslintConfigPrettier
-);
+	globalIgnores(["dist"]),
+	{
+      files: ["**/*.{ts}"],
+      languageOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        parserOptions: {
+          project: "./tsconfig.json",
+        },
+      },
+      extends: [
+        eslint.configs.recommended,
+        tseslint.configs.recommendedTypeChecked,
+        eslintConfigPrettier,
+      ],
+      rules: {
+        "no-console": "off",
+        "@typescript-eslint/no-floating-promises": "error",
+        "@typescript-eslint/no-misused-promises": "error",
+      }
+});
