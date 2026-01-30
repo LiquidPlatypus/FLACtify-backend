30211fbe29a11ed8fa8768d9a25fd780ed5c028e
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2026-01-30 16:30:49 +0100

    moved index.ts to src folder

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import fastify from "fastify";
 
+import {prisma} from "./prisma.js";
+
 const server = fastify();
 
 server.get("/ping", async (request, reply) => {
 	return "/pong\n";
 });
+
+server.get("/users", async() => {
+	return prisma.user.findMany();
+})
 
 server.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
 	if (err) {
