95ef466f8b35b092b1f542dbb9f3e499f080f347
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2026-03-04 14:37:34 +0100

    jwt config

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import fastify from "fastify";
+import jwt from "@fastify/jwt";
+import cookie from "@fastify/cookie";
 
-import {PrismaClient} from "../generated/prisma_client/client";
-import {PrismaPg} from "@prisma/adapter-pg";
+import { requireEnvs } from "./utils/env.js";
 
-const adapter = new PrismaPg({
-	connectionString: process.env.DATABASE_URL,
-});
+// @ts-ignore
+import { PrismaClient } from "../generated/prisma_client/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+import { userRoutes } from "./modules/user/user.route.js";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
 const server = fastify();
-const prisma = new PrismaClient({
-	adapter,
+const prisma = new PrismaClient({ adapter });
+
+server.decorate("prisma", prisma);
+
+server.addHook("onClose", async () => {
+	await prisma.$disconnect();
 });
 
 server.get("/", async (req, res) => {
@@ -17,9 +26,18 @@ server.get("/", async (req, res) => {
 	res.send(
 		userCount == 0
 			? "No users have been added yet."
-			: "Some users have been added to the database."
+			: "Some users have been added to the database.",
 	);
 });
+
+const { JWT_SECRET, COOKIE_SECRET } = requireEnvs([
+	"JWT_SECRET",
+	"COOKIE_SECRET",
+] as const);
+await server.register(jwt, { secret: JWT_SECRET });
+await server.register(cookie, { secret: COOKIE_SECRET });
+
+server.register(userRoutes, { prefix: "api/users" });
 
 server.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
 	if (err) {
